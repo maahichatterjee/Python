0814b490c018d5beeb10205d680818d441c3c9d0
Mahima Chatterjee <mahimachatterjee.info@gmail.com>
2026-01-11 17:09:57 +0530

    Add solution for Naming Conventions

---
+++ naming-conventions/solution.py
@@ -0,0 +1,9 @@
+"""
+Problem: Naming Conventions
+URL: https://neetcode.io/problems/python-naming-conventions/question
+Language: python
+
+Solution by NeetCode GitHub Pusher
+"""
+
+python_creation_date = "February, 1991"python_creation_date = "February, 1991"
