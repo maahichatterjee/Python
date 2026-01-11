44fc1488e3f1258adecd31684f8b2382e7bb3779
Mahima Chatterjee <mahimachatterjee.info@gmail.com>
2026-01-11 19:19:13 +0530

    Add solution for Dynamic Typing

---
+++ dynamic-typing/solution.py
@@ -0,0 +1,22 @@
+"""
+Problem: Dynamic Typing
+URL: https://neetcode.io/problems/python-dynamic-typing/question
+Language: python
+
+Solution by NeetCode GitHub Pusher
+"""
+
+variable = 10variable = 10
+print(type(variable))print(type(variable))
+
+print(type(variable))print(type(variable))
+
+print(type(variable))print(type(variable))
+
+print(type(variable))print(type(variable))
+
+print(type(variable))print(type(variable))
+variable = 2.2variable = 2.2
+variable = Truevariable = True
+variable = "maahi"variable = "maahi"
+variable = ["pizza", "burger","maggie"]variable = ["pizza", "burger","maggie"]
