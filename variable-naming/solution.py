b3aa3f829e803f485cd5e58e8216d875c684b4ed
Mahima Chatterjee <mahimachatterjee.info@gmail.com>
2026-01-11 16:56:46 +0530

    Update solution for Variable Naming

--- variable-naming/solution.py
+++ variable-naming/solution.py
@@ -7,10 +7,3 @@ Solution by NeetCode GitHub Pusher
 """
 
 capital_of_spain = "Madrid"capital_of_spain = "Madrid"
-capital_of_germany = "Berlin"capital_of_germany = "Berlin"
-
-
-print(capital_of_spain)print(capital_of_spain)
-print(capital_of_france)print(capital_of_france)
-print(capital_of_germany)print(capital_of_germany)
-capital_of_france = "Paris"capital_of_france = "Paris"
