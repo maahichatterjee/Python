dca5bccd8fb9b36fbea92e9ac4c1c297fa30ca81
Mahima Chatterjee <mahimachatterjee.info@gmail.com>
2026-01-11 16:07:59 +0530

    Add solution for Printing Text

---
+++ printing-text/solution.py
@@ -0,0 +1,13 @@
+"""
+Problem: Printing Text
+URL: https://neetcode.io/problems/python-printing-text/question
+Language: python
+
+Solution by NeetCode GitHub Pusher
+"""
+
+#print("My favorite quote is \"To be or not to be.\"")#print("My favorite quote is \"To be or not to be.\"")
+
+print ("My Favourite quote is 'to be or not be'")print ("My Favourite quote is 'to be or not be'")
+
+#alternate way:-#alternate way:-
