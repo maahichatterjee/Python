2aa8da7679769586eee086909010e2e1730e9cee
Mahima Chatterjee <mahimachatterjee.info@gmail.com>
2026-01-13 17:41:31 +0530

    Add solution for Empty Variable

---
+++ empty-variable/solution.py
@@ -0,0 +1,10 @@
+"""
+Problem: Empty Variable
+URL: https://neetcode.io/problems/python-empty-variable/question
+Language: python
+
+Solution by NeetCode GitHub Pusher
+"""
+
+var = None ;var = None ;
+print (typeof(var))print (typeof(var))
