a72509bd9bc03db1089cae0dafad5017a53e701d
Mahima Chatterjee <mahimachatterjee.info@gmail.com>
2026-01-13 20:54:56 +0530

    Add solution for More Operators

---
+++ more-operators/solution.py
@@ -0,0 +1,23 @@
+"""
+Problem: More Operators
+URL: https://neetcode.io/problems/python-more-operators/question
+Language: python
+
+Solution by NeetCode GitHub Pusher
+"""
+
+a, b, c = 2, 8, 5a, b, c = 2, 8, 5
+
+
+#1. The product of a and b, floor divided by c#1. The product of a and b, floor divided by c
+product = a*bproduct = a*b
+print (product // c)print (product // c)
+
+#2. The remainder of the product of a and b divided by c#2. The remainder of the product of a and b divided by c
+product % cproduct % c
+
+#3.The result of raising a to the power of b.#3.The result of raising a to the power of b.
+a ** ba ** b
+
+#4. The result of raising b to the power of c.#4. The result of raising b to the power of c.
+b ** cb ** c
