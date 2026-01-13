642801ca5653e7a8c1ec132645dd666847d6be75
Mahima Chatterjee <mahimachatterjee.info@gmail.com>
2026-01-13 20:40:04 +0530

    Add solution for Arithmetic Operators

---
+++ arithmetic-operators/solution.py
@@ -0,0 +1,19 @@
+"""
+Problem: Arithmetic Operators
+URL: https://neetcode.io/problems/python-arithmetic-operators/question
+Language: python
+
+Solution by NeetCode GitHub Pusher
+"""
+
+a, b, c = 2, 2, 0.5a, b, c = 2, 2, 0.5
+
+sum = a+b+c  #1sum = a+b+c  #1
+print(0-sum)  #2print(0-sum)  #2
+print(sum)   print(sum)   
+print(product) #3print(product) #3
+
+
+product = a*b*cproduct = a*b*c
+
+print(sum/product)print(sum/product)
