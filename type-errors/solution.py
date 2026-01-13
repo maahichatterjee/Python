226d71396c81f36bba34bbab4981e1759891bb2a
Mahima Chatterjee <mahimachatterjee.info@gmail.com>
2026-01-13 18:27:13 +0530

    Update solution for Type Errors

--- type-errors/solution.py
+++ type-errors/solution.py
@@ -6,7 +6,7 @@ Language: python
 Solution by NeetCode GitHub Pusher
 """
 
-variable = "10.2"variable = "10.2"
+variable = "10"variable = "10"
 
 # Don't modify the code below this line# Don't modify the code below this line
 print(variable)print(variable)
