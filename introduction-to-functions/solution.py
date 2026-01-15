17bad4cf09e0830228e461ba138d3873235ec09e
Mahima Chatterjee <mahimachatterjee.info@gmail.com>
2026-01-15 08:08:44 +0530

    Update solution for Introduction to Functions

--- introduction-to-functions/solution.py
+++ introduction-to-functions/solution.py
@@ -10,7 +10,7 @@ def greet():def greet():
     print("Hello, World!")    print("Hello, World!")
 
 def say_goodbye():def say_goodbye():
-    print("Goodbye, World")    print("Goodbye, World")
+    print("Goodbye, World!")    print("Goodbye, World!")
 
 # don't modify below this line# don't modify below this line
 greet()greet()
