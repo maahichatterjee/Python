f92577cc5c523f78b688f6e0e4a2046ea57b2523
Mahima Chatterjee <mahimachatterjee.info@gmail.com>
2026-01-15 09:37:29 +0530

    Update solution for Multiple Parameters

--- multiple-parameters/solution.py
+++ multiple-parameters/solution.py
@@ -7,15 +7,6 @@ Solution by NeetCode GitHub Pusher
 """
 
 def two_sum(num1, num2):def two_sum(num1, num2):
-
-two_sum(7, 10)two_sum(7, 10)
-
-
-# do not modify below this line# do not modify below this line
-two_sum(10, 9)two_sum(10, 9)
-three_sum(5, 14, 6)three_sum(5, 14, 6)
     print(num1 + num2)    print(num1 + num2)
 
 def three_sum(num3, num4, num5):def three_sum(num3, num4, num5):
-    print(num3 + num4 + num5)    print(num3 + num4 + num5)
-three_sum(3, 5, 8)three_sum(3, 5, 8)
