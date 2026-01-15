9528e29969f203883477e1a2bd9fdd0c005b48cf
Mahima Chatterjee <mahimachatterjee.info@gmail.com>
2026-01-15 08:15:10 +0530

    Add solution for Function Declaration

---
+++ function-declaration/solution.py
@@ -0,0 +1,13 @@
+"""
+Problem: Function Declaration
+URL: https://neetcode.io/problems/python-function-declaration/question
+Language: python
+
+Solution by NeetCode GitHub Pusher
+"""
+
+def print_number(n):def print_number(n):
+    print(n)    print(n)
+
+print_number(10)print_number(10)
+print_number(20)print_number(20)
