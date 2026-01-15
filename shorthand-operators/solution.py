baedb02610964608f2c00458286d8c67a2ebdc14
Mahima Chatterjee <mahimachatterjee.info@gmail.com>
2026-01-15 06:32:57 +0530

    Update solution for Shorthand Operators

--- shorthand-operators/solution.py
+++ shorthand-operators/solution.py
@@ -9,10 +9,13 @@ Solution by NeetCode GitHub Pusher
 number = 0number = 0
 
 #1. Add 5 to number and print the result.#1. Add 5 to number and print the result.
-print(number += 5)print(number += 5)
+number += 5number += 5
 
 #2.Subtract 2 from number and print the result.#2.Subtract 2 from number and print the result.
-print(number -= 2)print(number -= 2)
+number -= 2number -= 2
 
 #3. Add number to itself and print the result.#3. Add number to itself and print the result.
-print(number += number)print(number += number)
+print(number)print(number)
+print(number)print(number)
+number += numbernumber += number
+print(number)print(number)
