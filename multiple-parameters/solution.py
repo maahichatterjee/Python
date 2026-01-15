bdaab9118c910d63a236cc7585f4e75a201f9a0d
Mahima Chatterjee <mahimachatterjee.info@gmail.com>
2026-01-15 09:29:46 +0530

    Add solution for Multiple Parameters

---
+++ multiple-parameters/solution.py
@@ -0,0 +1,18 @@
+"""
+Problem: Multiple Parameters
+URL: https://neetcode.io/problems/python-multiple-parameters/question
+Language: python
+
+Solution by NeetCode GitHub Pusher
+"""
+
+def two_sum(num1 , num2):def two_sum(num1 , num2):
+
+def three_sum(num1, num2, num3):def three_sum(num1, num2, num3):
+
+
+# do not modify below this line# do not modify below this line
+two_sum(10, 9)two_sum(10, 9)
+three_sum(5, 14, 6)three_sum(5, 14, 6)
+    print(num1 + num2)    print(num1 + num2)
+    print(num1 + num2 + num3)    print(num1 + num2 + num3)
