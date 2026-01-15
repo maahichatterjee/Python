7dd50d27d5362f6164ada20c42ae8954baa2982b
Mahima Chatterjee <mahimachatterjee.info@gmail.com>
2026-01-15 06:59:28 +0530

    Add solution for Boolean Negation

---
+++ boolean-negation/solution.py
@@ -0,0 +1,13 @@
+"""
+Problem: Boolean Negation
+URL: https://neetcode.io/problems/python-boolean-negation/question
+Language: python
+
+Solution by NeetCode GitHub Pusher
+"""
+
+a, b, c = False, False, Truea, b, c = False, False, True
+print (not a)print (not a)
+print(not c)print(not c)
+print(not (a and b))print(not (a and b))
+print(not (b or c))print(not (b or c))
