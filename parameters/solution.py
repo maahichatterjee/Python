7ae8f9421569723de3c4d151900c94a683acd2b5
Mahima Chatterjee <mahimachatterjee.info@gmail.com>
2026-01-15 09:11:23 +0530

    Update solution for Parameters

--- parameters/solution.py
+++ parameters/solution.py
@@ -7,9 +7,9 @@ Solution by NeetCode GitHub Pusher
 """
 
 def farewell(name):def farewell(name):
-    print("Goodbye," + name)    print("Goodbye," + name)
+    print("Goodbye, " + name)    print("Goodbye, " + name)
 
-farewell("Bob")farewell("Bob")
-farewell("Charlie")farewell("Charlie")
+farewell("Bob ")farewell("Bob ")
+farewell("Charlie ")farewell("Charlie ")
 # don't modify below this line# don't modify below this line
-farewell("NeetCode")farewell("NeetCode")
+farewell("NeetCode ")farewell("NeetCode ")
