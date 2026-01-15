3f3ad01c0dbbd0c8972a3b6ea63788b87234384b
Mahima Chatterjee <mahimachatterjee.info@gmail.com>
2026-01-15 10:10:40 +0530

    Add solution for Type Hints

---
+++ type-hints/solution.py
@@ -0,0 +1,11 @@
+"""
+Problem: Type Hints
+URL: https://neetcode.io/problems/python-type-hints/question
+Language: python
+
+Solution by NeetCode GitHub Pusher
+"""
+
+def greet(name: str) -> None:def greet(name: str) -> None:
+    print("Hello, " + name)    print("Hello, " + name)
+
