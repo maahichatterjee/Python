6b405a7bfc136ee78a4ce160147146e917b27ee1
Mahima Chatterjee <mahimachatterjee.info@gmail.com>
2026-01-15 09:56:26 +0530

    Add solution for Return Statement

---
+++ return-statement/solution.py
@@ -0,0 +1,14 @@
+"""
+Problem: Return Statement
+URL: https://neetcode.io/problems/python-return-statement/question
+Language: python
+
+Solution by NeetCode GitHub Pusher
+"""
+
+def product(a, b):def product(a, b):
+
+
+
+# don't modify below this line# don't modify below this line
+    return a*b    return a*b
