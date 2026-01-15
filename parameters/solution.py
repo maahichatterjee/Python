f08d44cfa516de43c21f6cb39c93f6778becb80a
Mahima Chatterjee <mahimachatterjee.info@gmail.com>
2026-01-15 09:10:18 +0530

    Add solution for Parameters

---
+++ parameters/solution.py
@@ -0,0 +1,15 @@
+"""
+Problem: Parameters
+URL: https://neetcode.io/problems/python-parameters/question
+Language: python
+
+Solution by NeetCode GitHub Pusher
+"""
+
+def farewell(name):def farewell(name):
+
+farewell("Bob")farewell("Bob")
+# don't modify below this line# don't modify below this line
+farewell("NeetCode")farewell("NeetCode")
+    print("Goodbye,"+ name)    print("Goodbye,"+ name)
+farewell("Charlie")farewell("Charlie")
