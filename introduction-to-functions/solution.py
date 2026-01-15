0fab0fb3ce5aa545ad6755938a2bcafb8f1e0edc
Mahima Chatterjee <mahimachatterjee.info@gmail.com>
2026-01-15 08:07:51 +0530

    Add solution for Introduction to Functions

---
+++ introduction-to-functions/solution.py
@@ -0,0 +1,17 @@
+"""
+Problem: Introduction to Functions
+URL: https://neetcode.io/problems/python-introduction-to-functions/question
+Language: python
+
+Solution by NeetCode GitHub Pusher
+"""
+
+def greet():def greet():
+    print("Hello, World!")    print("Hello, World!")
+
+def say_goodbye():def say_goodbye():
+
+# don't modify below this line# don't modify below this line
+greet()greet()
+say_goodbye()say_goodbye()
+    print("goodbye, world")    print("goodbye, world")
