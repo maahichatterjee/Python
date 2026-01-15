61feb69fecdacbe4e8da13e90b8d68c7507ed9a3
Mahima Chatterjee <mahimachatterjee.info@gmail.com>
2026-01-15 10:13:30 +0530

    Update solution for Type Hints

--- type-hints/solution.py
+++ type-hints/solution.py
@@ -10,4 +10,4 @@ def greet(name: str) -> None:def greet(name: str) -> None:
     print("Hello, " + name)    print("Hello, " + name)
 
 greet("NeetCode")greet("NeetCode")
-type()type()
+type(greet)type(greet)
