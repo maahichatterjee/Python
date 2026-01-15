8be139d1858be36415a565c0c5c6196cc5ab40b2
Mahima Chatterjee <mahimachatterjee.info@gmail.com>
2026-01-15 06:45:34 +0530

    Add solution for Boolean OR

---
+++ boolean-or/solution.py
@@ -0,0 +1,14 @@
+"""
+Problem: Boolean OR
+URL: https://neetcode.io/problems/python-boolean-or/question
+Language: python
+
+Solution by NeetCode GitHub Pusher
+"""
+
+a, b, c, d = False, False, True, Truea, b, c, d = False, False, True, True
+
+print (a or b)print (a or b)
+print (b or c)print (b or c)
+print(c or d)print(c or d)
+print (a or b or c or d)print (a or b or c or d)
