54aaddde63af3415e8ff80ba342cde9b648cdf64
Mahima Chatterjee <mahimachatterjee.info@gmail.com>
2026-01-15 06:51:15 +0530

    Add solution for Boolean AND

---
+++ boolean-and/solution.py
@@ -0,0 +1,14 @@
+"""
+Problem: Boolean AND
+URL: https://neetcode.io/problems/python-boolean-and/question
+Language: python
+
+Solution by NeetCode GitHub Pusher
+"""
+
+a, b, c, d = False, False, True, Truea, b, c, d = False, False, True, True
+
+print(a and b)print(a and b)
+print (b and c)print (b and c)
+print (c and d)print (c and d)
+print (a and b and c and d)print (a and b and c and d)
