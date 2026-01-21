ead479584441a9c523eebf1d3a6116f22dff4add
Mahima Chatterjee <mahimachatterjee.info@gmail.com>
2026-01-21 09:38:30 +0530

    Add solution for Default Arguments

---
+++ default-arguments/solution.py
@@ -0,0 +1,9 @@
+"""
+Problem: Default Arguments
+URL: https://neetcode.io/problems/python-default-arguments/question
+Language: python
+
+Solution by NeetCode GitHub Pusher
+"""
+
+def greet(name, punctuation = "!") -> None:def greet(name, punctuation = "!") -> None:
