319b94f7c3d30b14ce453885966bb3f7d6fb63d1
Mahima Chatterjee <mahimachatterjee.info@gmail.com>
2026-01-26 18:24:20 +0530

    Add solution for If Statements

---
+++ if-statements/solution.py
@@ -0,0 +1,9 @@
+"""
+Problem: If Statements
+URL: https://neetcode.io/problems/python-if/question
+Language: python
+
+Solution by NeetCode GitHub Pusher
+"""
+
+def is_balance_low(balance: int) -> None: def is_balance_low(balance: int) -> None: 
