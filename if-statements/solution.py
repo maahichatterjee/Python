20ee14ce86be8dd2115e1a77755e80d66adbf348
Mahima Chatterjee <mahimachatterjee.info@gmail.com>
2026-01-26 18:48:35 +0530

    Update solution for If Statements

--- if-statements/solution.py
+++ if-statements/solution.py
@@ -6,4 +6,10 @@ Language: python
 Solution by NeetCode GitHub Pusher
 """
 
-def is_balance_low(balance: int) -> None: def is_balance_low(balance: int) -> None: 
+def is_balance_low(balance: int) -> None:def is_balance_low(balance: int) -> None:
+    if balance <= 100:    if balance <= 100:
+        print("Warning: Low balance.")        print("Warning: Low balance.")
+        
+# do not modify below this line# do not modify below this line
+is_balance_low(99)is_balance_low(99)
+is_balance_low(100)is_balance_low(100)
