a5195c810eb11bc93fdf0ef2eb116ec7575ba7b8
Mahima Chatterjee <mahimachatterjee.info@gmail.com>
2026-01-27 10:30:28 +0530

    Add solution for If-Else Statements

---
+++ if-else-statements/solution.py
@@ -0,0 +1,9 @@
+"""
+Problem: If-Else Statements
+URL: https://neetcode.io/problems/python-if-else/question
+Language: python
+
+Solution by NeetCode GitHub Pusher
+"""
+
+def get_min(a: int, b: int) -> int:def get_min(a: int, b: int) -> int:
