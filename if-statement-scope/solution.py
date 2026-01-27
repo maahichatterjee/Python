bfc87c02846f9fdc7bce96072740e7bcb4542d55
Mahima Chatterjee <mahimachatterjee.info@gmail.com>
2026-01-27 09:34:36 +0530

    Add solution for If Statement Scope

---
+++ if-statement-scope/solution.py
@@ -0,0 +1,9 @@
+"""
+Problem: If Statement Scope
+URL: https://neetcode.io/problems/python-if-scope/question
+Language: python
+
+Solution by NeetCode GitHub Pusher
+"""
+
+def pay_bill(balance: int, bill: int) -> int:def pay_bill(balance: int, bill: int) -> int:
