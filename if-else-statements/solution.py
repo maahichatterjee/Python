11c11b110c4480a20d700b70a2a4286928ca46c6
Mahima Chatterjee <mahimachatterjee.info@gmail.com>
2026-01-27 10:31:03 +0530

    Update solution for If-Else Statements

--- if-else-statements/solution.py
+++ if-else-statements/solution.py
@@ -7,3 +7,12 @@ Solution by NeetCode GitHub Pusher
 """
 
 def get_min(a: int, b: int) -> int:def get_min(a: int, b: int) -> int:
+    if a>b:    if a>b:
+        return b        return b
+    else:    else:
+        return a        return a
+
+# don't modify code below this line# don't modify code below this line
+print(get_min(10, 11))print(get_min(10, 11))
+print(get_min(5, -7))print(get_min(5, -7))
+print(get_min(20, 20))print(get_min(20, 20))
