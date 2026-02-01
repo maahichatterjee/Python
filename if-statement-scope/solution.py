9c1ff9c6d749576a5c6b65d663b1dcf99ba5762d
Mahima Chatterjee <mahimachatterjee.info@gmail.com>
2026-02-01 13:29:59 +0530

    Update solution for If Statement Scope

--- if-statement-scope/solution.py
+++ if-statement-scope/solution.py
@@ -7,11 +7,3 @@ Solution by NeetCode GitHub Pusher
 """
 
 def pay_bill(balance: int, bill: int) -> int:def pay_bill(balance: int, bill: int) -> int:
-    if balance>=bill:    if balance>=bill:
-        balance = balance-bill        balance = balance-bill
-        
-    return balance    return balance
-# do not modify below this line# do not modify below this line
-print(pay_bill(100, 50))print(pay_bill(100, 50))
-print(pay_bill(100, 100))print(pay_bill(100, 100))
-print(pay_bill(100, 150))print(pay_bill(100, 150))
