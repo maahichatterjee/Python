71f0576816ea7cdb925a43b672d4e744a23dc19d
Mahima Chatterjee <mahimachatterjee.info@gmail.com>
2026-02-01 18:02:12 +0530

    Add solution for Else-If Statements

---
+++ else-if-statements/solution.py
@@ -0,0 +1,9 @@
+"""
+Problem: Else-If Statements
+URL: https://neetcode.io/problems/python-elif/question
+Language: python
+
+Solution by NeetCode GitHub Pusher
+"""
+
+def check_range(num: int) -> str:def check_range(num: int) -> str:
