213a96789a2ca1b0d6acc383d09e63f8373ff84a
Mahima Chatterjee <mahimachatterjee.info@gmail.com>
2026-02-06 09:23:41 +0530

    Update solution for Default Arguments

--- default-arguments/solution.py
+++ default-arguments/solution.py
@@ -6,4 +6,8 @@ Language: python
 Solution by NeetCode GitHub Pusher
 """
 
-def greet(name,!) -> None:def greet(name,!) -> None:
+def greet(name, punctuation="!") -> None:def greet(name, punctuation="!") -> None:
+    print("Hello, " + name + punctuation)    print("Hello, " + name + punctuation)
+
+greet("World", "!")greet("World", "!")
+greet("World")greet("World")
